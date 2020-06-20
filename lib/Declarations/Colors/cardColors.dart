eaa543fcde1ce282cb5d4f6a0700dc7bfb7dc6e8
Vijay <vijaythevirtue@gmail.com>
2020-06-20 17:51:24 +0530

    Dashboard Implementation | Status : 🆗

---
+++ lib/Declarations/Colors/cardColors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+List<Color> cardcolorsleft = [
+  Colors.blue[50],
+  Colors.red[50],
+  Colors.green[50],
+  Colors.pink[50],
+  Colors.orange[50],
+  Colors.indigoAccent[50],
+  Colors.yellow[50],
+  Colors.brown[50],
+];
+List<Color> cardcolorsright = [
+  Colors.brown[50],
+  Colors.yellow[50],
+  Colors.indigoAccent[50],
+  Colors.orange[50],
+  Colors.pink[50],
+  Colors.pink[50],
+  Colors.green[50],
+  Colors.red[50],
+  Colors.blue[50],
+];
---
+++ lib/Declarations/Images.dart
@@ -0,0 +1,25 @@
+final List<String> horizontalCardImages = [
+  "assets/Card_horizontal/vegetables.jpg",
+  "assets/Card_horizontal/drinks.jpg",
+  "assets/Card_horizontal/cosmetics.jpg",
+  "assets/Card_horizontal/soap.jpg",
+];
+final List<String> carouselimages = [
+  "assets/Carousel_images/AffordablePrice.png",
+  "assets/Carousel_images/happyitems.png",
+  "assets/Carousel_images/homedelivery.png",
+  "assets/Carousel_images/customers.png",
+  "assets/Carousel_images/discount.png",
+];
+final List<String> verticalCardImagesleft = [
+  "assets/Card_Vertical/Vegetables.png",
+  "assets/Card_Vertical/snacks.png",
+  "assets/Card_Vertical/powder.png",
+  "assets/Card_Vertical/Grains.png",
+];
+final List<String> verticalCardImagesright = [
+  "assets/Card_Vertical/cake.png",
+  "assets/Card_Vertical/beautyitems.png",
+  "assets/Card_Vertical/leatheritems.png",
+  "assets/Card_Vertical/beautyitem.png",
+];

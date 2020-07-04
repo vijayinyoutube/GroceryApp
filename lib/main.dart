bc21d2257fdd7788d8c76b72c6cf0ea497e9ffe4
Vijay R <58719230+vijayinyoutube@users.noreply.github.com>
2020-07-04 15:26:14 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
-      home: InfoPage(),
+      home: DashboardPage(),
       routes: {
         "/DashboardPage": (_) => new DashboardPage(),
       },
